d0e4ec9acd4d113d150aea4402145f0d03ec6c5a
Ritsuka07 <lanhuong171203@gmail.com>
2023-04-21 01:29:27 +0700

    comment

--- app/src/main/java/com/example/bookapp/BookDetailActivity.kt
+++ app/src/main/java/com/example/bookapp/BookDetailActivity.kt
@@ -1,16 +1,22 @@
 package com.example.bookapp
 
 import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bookapp.Adapter.AdapterChapterAdmin
+import com.example.bookapp.Adapter.AdapterComment
 import com.example.bookapp.Func.MyApplication
 import com.example.bookapp.Model.ModelChapter
+import com.example.bookapp.Model.ModelComment
 import com.example.bookapp.databinding.ActivityBookDetailBinding
+import com.example.bookapp.databinding.DialogCommentAddBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -20,9 +26,15 @@ import com.google.firebase.database.ValueEventListener
 class BookDetailActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityBookDetailBinding
+
     private lateinit var chapterArrayList: ArrayList<ModelChapter>
     private lateinit var adapterChapter: AdapterChapterAdmin
+
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
+    private lateinit var commentArrayList: ArrayList<ModelComment>
+    private lateinit var adapterComment: AdapterComment
 
     private companion object {
         const val TAG = "BOOK_DETAIL_TAG"
@@ -39,12 +51,24 @@ class BookDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+
         firebaseAuth = FirebaseAuth.getInstance()
 
         bookId = intent.getStringExtra("bookId")!!
 
         binding.backBtn.setOnClickListener {
             onBackPressed()
+        }
+
+        binding.commentBtn.setOnClickListener {
+            if (firebaseAuth.currentUser == null) {
+                Toast.makeText(this, "You're not logging!", Toast.LENGTH_SHORT).show()
+            } else {
+                addCommentDialog()
+            }
         }
 
         binding.addFavorite.setOnClickListener {
@@ -62,6 +86,91 @@ class BookDetailActivity : AppCompatActivity() {
         checkFavorite()
         loadBookDetail()
         loadBookChapter()
+        loadComment()
+    }
+
+    private fun loadComment() {
+        commentArrayList = ArrayList()
+        val ref = FirebaseDatabase.getInstance().getReference("Books")
+        ref.child(bookId).child("Comments")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    //clear list
+                    commentArrayList.clear()
+                    //get data
+                    for (ds in snapshot.children) {
+                        val model = ds.getValue(ModelComment::class.java)
+                        //add to list
+                        if (model != null) {
+                            commentArrayList.add(model)
+                        }
+
+                    }
+
+
+                    val layoutManager = LinearLayoutManager(this@BookDetailActivity)
+                    binding.listComments.layoutManager = layoutManager
+
+                    if (commentArrayList.isNotEmpty()) {
+                        adapterComment = AdapterComment(this@BookDetailActivity, commentArrayList)
+                        binding.listComments.adapter = adapterComment
+                    } else {
+                        //...
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+    }
+
+    private var comment = ""
+
+    private fun addCommentDialog() {
+        val commentAddBinding = DialogCommentAddBinding.inflate(LayoutInflater.from(this))
+        val builder = AlertDialog.Builder(this, R.style.CustomDialog)
+        builder.setView(commentAddBinding.root)
+
+        val alertDialog = builder.create()
+        alertDialog.show()
+
+        commentAddBinding.backBtn.setOnClickListener {
+            onBackPressed()
+        }
+        commentAddBinding.submitComment.setOnClickListener {
+            comment = commentAddBinding.tvComment.text.toString().trim()
+            if (comment.isEmpty()) {
+                Toast.makeText(this, "Enter comment", Toast.LENGTH_SHORT).show()
+            } else {
+                alertDialog.dismiss()
+                addComment()
+            }
+        }
+    }
+
+    private fun addComment() {
+        val timestamp = "${System.currentTimeMillis()}"
+        val hashMap = HashMap<String, Any>()
+
+        hashMap["id"] = "$timestamp"
+        hashMap["bookId"] = "$bookId"
+        hashMap["timestamp"] = "$timestamp"
+        hashMap["comment"] = "$comment"
+        hashMap["uid"] = "${firebaseAuth.uid}"
+
+        val ref = FirebaseDatabase.getInstance().getReference("Books")
+        ref.child(bookId).child("Comments").child(timestamp)
+            .setValue(hashMap)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(this, "Comment added", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e->
+                progressDialog.dismiss()
+                Toast.makeText(this, "Failed: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun checkFavorite() {
